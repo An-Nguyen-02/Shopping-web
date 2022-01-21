8403befe6d6f0610dc81f636b797d2def5141abb
An Nguyen <an.nguyen@tuni.fi>
2022-01-21 15:50:30 +0200

    add navigation bar

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,14 +2,12 @@ import './App.css';
 import { Route, Switch } from 'react-router-dom';
 import Homepage from '../pages/Homepage/Homepage.js';
 import ShopPage from '../pages/Shop/shop.js';
-const HatsPage = ()=>{
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-}
+import Header from '../components/header/header.js';
+
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={Homepage}/>
         <Route path='/shop' component={ShopPage}/>
