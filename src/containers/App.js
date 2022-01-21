4a43f4c9bc262a775c3c41c3f4f15f22c5ba44bf
An Nguyen <an.nguyen@tuni.fi>
2022-01-21 15:29:11 +0200

    add shopping page + basic style

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,11 +1,19 @@
 import './App.css';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import Homepage from '../pages/Homepage/Homepage.js';
-
+import ShopPage from '../pages/Shop/shop.js';
+const HatsPage = ()=>{
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+}
 function App() {
   return (
     <div>
-      <Homepage />
+      <Switch>
+        <Route exact path='/' component={Homepage}/>
+        <Route path='/shop' component={ShopPage}/>
+      </Switch>
     </div>
   );
 }
---
+++ src/pages/Shop/shop.js
@@ -0,0 +1,28 @@
+import react from "react";
+import SHOP_DATA from './shopData.js';
+import CollectionPreview from '../../components/collection-preview/collection-preview.js'
+
+class ShopPage extends react.Component {
+    constructor(props){
+        super(props);
+
+        this.state ={
+            collections: SHOP_DATA
+        }
+    }
+
+    render(){
+        const {collections} = this.state;
+        return(
+            <div className="shop-page">
+                {
+                    collections.map(({id, ...otherCollectionProps}) => (
+                        <CollectionPreview key={id} {...otherCollectionProps} />
+                    ))
+                }
+            </div>
+        )
+    }
+}
+
+export default ShopPage;
