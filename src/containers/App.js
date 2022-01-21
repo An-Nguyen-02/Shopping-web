1e1fd8c538b01f91a5a789c4dc3656ebea52f700
An Nguyen <an.nguyen@tuni.fi>
2022-01-21 18:33:33 +0200

    add basic sign-in form

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { Route, Switch } from 'react-router-dom';
 import Homepage from '../pages/Homepage/Homepage.js';
 import ShopPage from '../pages/Shop/shop.js';
+import SigInAndSignUp from '../pages/SignIn-Up/signIn-Up.js';
 import Header from '../components/header/header.js';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={Homepage}/>
         <Route path='/shop' component={ShopPage}/>
+        <Route path='/signin' component={SigInAndSignUp}/>
       </Switch>
     </div>
   );
---
+++ src/pages/SignIn-Up/signIn-Up.js
@@ -0,0 +1,12 @@
+import react from "react";
+import SignIn from '../../components/sign-in/sign-in.js'
+
+import './signIn-Up.scss';
+
+const SigInAndSignUp = () => (
+    <div className="sign-in-up">
+        <SignIn />
+    </div>
+)
+
+export default SigInAndSignUp;
