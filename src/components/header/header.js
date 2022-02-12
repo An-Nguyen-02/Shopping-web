0d7e199c78860b6e3563cbc8d15e7ecf7cd81c76
An Nguyen <nhatan020902@gmail.com>
2022-02-12 15:49:24 +0200

    add cart icon to end of header

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -4,6 +4,7 @@ import { connect } from "react-redux";
 import { Link } from "react-router-dom/cjs/react-router-dom.min";
 import { ReactComponent as Logo } from "../../assets/logo.svg";
 import { auth } from "../../firebase/firebase.util.js";
+import CartIcon from "../cart-icon/cart-icon.component";
 
 const Header = ({ currentUser }) => (
   <div className="header">
@@ -29,6 +30,7 @@ const Header = ({ currentUser }) => (
           </Link>
         )}
       </Link>
+      <CartIcon />
     </div>
   </div>
 );
