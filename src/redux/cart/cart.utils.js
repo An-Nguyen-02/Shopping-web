9197c24d62e8d5ada330c3d5042946f073b4c111
An Nguyen <nhatan020902@gmail.com>
2022-02-12 21:31:37 +0200

    finnish cart dropdown feature

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -5,7 +5,7 @@ export const addItemToCart = (cartItems, cartItemsToAdd) => {
   if (existingCartItem) {
     return cartItems.map((item) =>
       item.id === cartItemsToAdd.id
-        ? { ...cartItems, quantity: item.quantity + 1 }
+        ? { ...item, quantity: item.quantity + 1 }
         : item
     );
   }
