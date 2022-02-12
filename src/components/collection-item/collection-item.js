7f208a10788a5687b83861a8c565fced289125eb
An Nguyen <nhatan020902@gmail.com>
2022-02-12 16:42:45 +0200

    add hover to item and add to cart button

--- src/components/collection-item/collection-item.js
+++ src/components/collection-item/collection-item.js
@@ -1,18 +1,20 @@
 import react from "react";
-import './collection-item.scss';
-
-const CollectionItem = ({id, name, price, imageUrl }) =>(
-    <div className="collection-item">
-        <div 
-            className="image"
-            style={{
-                backgroundImage: `url(${imageUrl})`
-            }}/>
-        <div className="collection-footer">
-            <span className="name">{name}</span>
-            <span className="price">{price}</span>
-        </div>
+import "./collection-item.scss";
+import CustomButton from "../custom-button/custom-button";
+const CollectionItem = ({ id, name, price, imageUrl }) => (
+  <div className="collection-item">
+    <div
+      className="image"
+      style={{
+        backgroundImage: `url(${imageUrl})`,
+      }}
+    />
+    <div className="collection-footer">
+      <span className="name">{name}</span>
+      <span className="price">{price}</span>
     </div>
-)
+    <CustomButton inverted>Add to cart</CustomButton>
+  </div>
+);
 
 export default CollectionItem;
