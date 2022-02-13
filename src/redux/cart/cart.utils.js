f814103377a01e4b06b6b0a88dac38b8da0c4823
An Nguyen <nhatan020902@gmail.com>
2022-02-13 12:34:55 +0200

    add cart to local storage to maintain session

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -13,7 +13,7 @@ export const addItemToCart = (cartItems, cartItemsToAdd) => {
 };
 
 export const removeItemFromCart = (cartItems, cartItemToRemove) => {
-  return cartItems.filter((cartItem) => cartItem.id != cartItemToRemove.id);
+  return cartItems.filter((cartItem) => cartItem.id !== cartItemToRemove.id);
 };
 
 export const decreaseItemQuant = (cartItems, cartItemToDecrease) => {
@@ -21,7 +21,9 @@ export const decreaseItemQuant = (cartItems, cartItemToDecrease) => {
     (cartItem) => cartItem.id === cartItemToDecrease.id
   );
   if (existingCartItem.quantity === 1) {
-    return cartItems.filter((cartItem) => cartItem.id != cartItemToDecrease.id);
+    return cartItems.filter(
+      (cartItem) => cartItem.id !== cartItemToDecrease.id
+    );
   }
   return cartItems.map((item) =>
     item.id === cartItemToDecrease.id
