cdcf48da42eab571ead90a9dae68e21180e5f1fe
An Nguyen <nhatan020902@gmail.com>
2022-02-19 13:00:19 +0200

    seperate front-end to client folder and add express backend for payment

--- client/src/components/stripe-button/stripe-button.component.js
+++ client/src/components/stripe-button/stripe-button.component.js
@@ -1,9 +1,28 @@
 import StripeCheckout from "react-stripe-checkout";
-
+import axios from "axios";
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
   const publishableKey =
     "pk_test_51KSjpCBYp3P0VPu4qlVoSK2SwYMcS0HMwJmI8bPOkJFGDtxyrgYvz2EZNKBeb8n916k41ucT3Wu9qKI3q3QWpklJ00khAWF3Xz";
+  const onToken = (token) => {
+    axios({
+      url: "payment",
+      method: "post",
+      data: {
+        amount: priceForStripe,
+        token: token,
+      },
+    })
+      .then((res) => {
+        alert("Payment successful");
+      })
+      .catch((error) => {
+        console.log("Payment error", JSON.parse(error));
+        alert(
+          "There was an issue with your payment. Please make sure you provided the test credit card."
+        );
+      });
+  };
   return (
     <StripeCheckout
       label="Pay Now"
@@ -14,9 +33,7 @@ const StripeCheckoutButton = ({ price }) => {
       description={`Your total payment is $${price}`}
       amount={priceForStripe}
       panelLabel="Pay Now"
-      token={() => {
-        alert("Payment Successful");
-      }}
+      token={onToken}
       stripeKey={publishableKey}
     />
   );
